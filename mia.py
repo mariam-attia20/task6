3539547ee3ef4de4ca91137bfb658bf10dca1a6b
mariam attia <mariamattia94.75@gmail.com>
2025-08-08 22:03:43 +0200

    Check first letter

---
+++ mia.py
@@ -0,0 +1,12 @@
+import os
+import random
+
+correct = "SHARK"
+guess = input("plz guess >> ")
+
+if guess[0] == correct[0]:
+    print(f"{guess[0]}" ,  end = " ")
+elif guess[0] in correct:
+    print(f"{guess[0]}" , end = " ")
+else:
+    print(guess[0] , end=" ")
