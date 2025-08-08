28ea80374621601187992a16ace6385e11e4718d
mariam attia <mariamattia94.75@gmail.com>
2025-08-08 22:06:46 +0200

    check all word

--- mia.py
+++ mia.py
@@ -4,9 +4,10 @@ import random
 correct = "SHARK"
 guess = input("plz guess >> ")
 
-if guess[0] == correct[0]:
-    print(f"{guess[0]}" ,  end = " ")
-elif guess[0] in correct:
-    print(f"{guess[0]}" , end = " ")
-else:
-    print(guess[0] , end=" ")
+for i in range(5):
+   if guess[0] == correct[0]:
+        print(f"{guess[0]}" ,  end = " ")
+   elif guess[0] in correct:
+        print(f"{guess[0]}" , end = " ")
+   else:
+         print(guess[0] , end=" ")
