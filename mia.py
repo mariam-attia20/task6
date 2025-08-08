14a10660f1b8fd7405d8fb27c87d9656af050b64
mariam attia <mariamattia94.75@gmail.com>
2025-08-08 22:36:28 +0200

    add condition if user win or lose

--- mia.py
+++ mia.py
@@ -6,14 +6,24 @@ green = "\033[42m"
 yellow = "\033[43m"
 rest = "\033[0m"
 
-correct = "SHARK"
+correct = random.choice(["SHARK" , "PLANE" , "ALARM", "ROBOT" , "EARTH", "PIZZA"])
 
 for h in range(5):#5 chance
-  guess = input("plz guess >> ").upper()
-  for i in range(5):#check the word
-     if guess[i] == correct[i]:
-          print(f"{green}{guess[i]}{rest}" ,  end = " ")
-     elif guess[i] in correct:
-          print(f"{yellow}{guess[i]}{rest}" , end = " ")
-     else:
-          print(guess[i] , end=" ")
+    guess = input("plz guess >> ").upper()
+    for i in range(5):#check the word
+        if guess[i] == correct[i]:
+           print(f"{green}{guess[i]}{rest}" ,  end = " ")
+        elif guess[i] in correct:
+            print(f"{yellow}{guess[i]}{rest}" , end = " ")
+        else:
+            print(guess[i] , end=" ")
+    print(" ")
+       
+    if guess == correct:
+           print("YOU LOSE")
+           exit()
+           
+print("YOU LOSE !")
+print(f" the word is {correct}")
+       
+      
